5afe351b973f5b095e49d7334048e9f217952839
Nhatbao1 <91550513+Nhatbao1@users.noreply.github.com>
2022-12-03 16:07:58 +0700

    Update axiosCustomize.js

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -6,8 +6,7 @@ nProgress.configure({
     trickleSpeed: 100,
 })
 const instance = axios.create({
-    baseURL: 'http://localhost:8081/',
-    // no se cong them /api/ trong file apiService de chay
+    baseURL: 'http://localhost:8081/',cors:true
 });
 // interceptor se chen chan vao moi lan trinh duyet gui request va sau moi lan sever gui respone
 // Add a request interceptor
